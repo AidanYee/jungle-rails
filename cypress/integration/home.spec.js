25f2c16f63c4f329a16a0fe55c44443706fe642c
Aidan Yee <aidan.yee@pm.me>
2022-06-07 21:00:34 +0000

    add X to prevent testing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,13 +1,13 @@
-describe("home.spec.js", () => {
+xdescribe("home.spec.js", () => {
   before(() => {
     cy.visit("0.0.0.0:3000");
   });
 
-  it("There is products on the page", () => {
+  xit("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
 
-  it("There is 12 products on the page", () => {
+  xit("There is 12 products on the page", () => {
     cy.get(".products article").should("have.length", 12);
   });
   
--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,9 +1,9 @@
-describe("product_details.spec.js", () => {
+xdescribe("product_details.spec.js", () => {
   before(() => {
     cy.visit("0.0.0.0:3000");
   });
 
-  it("navigate from the home page to the product detail page", () => {
+  xit("navigate from the home page to the product detail page", () => {
     cy.get(".products article")
       .first()
       .click();
