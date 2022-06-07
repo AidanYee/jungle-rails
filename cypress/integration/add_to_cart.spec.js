3053103d084ba5e0bb7b8088c79003ec4a306861
Aidan Yee <aidan.yee@pm.me>
2022-06-07 21:40:30 +0000

    change text to search for

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -4,10 +4,10 @@ describe("add_to_cart.spec.js", () => {
   });
 
   it("Verify the total amount in cart after clicking add to cart button", () => {
-    cy.get("li.nav-item.end-0").should("include.text", "\n           \n          My Cart (0) \n        ");
+    cy.get("li.nav-item.end-0").should("include.text", "My Cart (0)");
   
     cy.contains("button", "Add").first().click({ force: true });
 
-    cy.get("li.nav-item.end-0").should("include.text", "\n           \n          My Cart (1) \n        ");
+    cy.get("li.nav-item.end-0").should("include.text", "My Cart (1)");
   });
 });
