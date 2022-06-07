f35b78f99c00f76325dbbad6d2d41f56a34448b7
Aidan Yee <aidan.yee@pm.me>
2022-06-07 01:59:58 +0000

    update formatting

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,8 +10,7 @@ RSpec.describe Product, type: :model do
         name: 'Super Duke 1290R',
         price: 300,
         quantity: 1,
-        category: @category
-      )
+        category: @category)
     end
 
     it "should have a name" do
@@ -19,9 +18,7 @@ RSpec.describe Product, type: :model do
         name: nil,
         price: 300,
         quantity: 1,
-        category: @category
-      )
-
+        category: @category)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include 'Name can\'t be blank'
     end
@@ -31,9 +28,7 @@ RSpec.describe Product, type: :model do
         name: 'S1000RR',
         price: nil,
         quantity: 1,
-        category: @category
-      )
-
+        category: @category)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include 'Category can\'t be blank'
     end
@@ -43,9 +38,7 @@ RSpec.describe Product, type: :model do
         name: 'GSX-R 1000',
         price: nil,
         quantity: nil,
-        category: @category
-      )
-
+        category: @category)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include 'Quantity can\'t be blank'
     end
@@ -55,9 +48,7 @@ RSpec.describe Product, type: :model do
         name: 'YZF-R1',
         price: nil,
         quantity: nil,
-        category: nil
-      )
-
+        category: nil)
       expect(product).to_not be_valid
       expect(product.errors.full_messages).to include 'Category can\'t be blank'
     end
