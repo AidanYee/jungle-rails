76ebf22298af3fe3b697d187e973b2e29d001967
Aidan Yee <aidan.yee@pm.me>
2022-06-07 20:29:35 +0000

    fix issue with products on home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,14 +1,14 @@
 describe("home.spec.js", () => {
-  it("Should visit the homepage", () => {
-    cy.visit('0.0.0.0:3000');
+  before(() => {
+    cy.visit("0.0.0.0:3000");
   });
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
 
-  it("There is 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
   });
   
 });
